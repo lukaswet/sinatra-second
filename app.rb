9190df1ae623594b899f58cbcd6b992c3c9951cd
ЛУКАС <lukaswet@gmail.com>
2016-08-17 15:25:03 +0000

    saving Client to DB on Visit view

--- app.rb
+++ app.rb
@@ -1,8 +1,36 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base
+    
+end
+
+
+class Barber < ActiveRecord::Base
+    
+end
+
+before do
+	@barbers = Barber.all
+end
+
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index			
 end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	c = Client.new params[:client]
+	c.save
+
+	erb "<h2>Спасибо, вы записались</h2>"
+end
+
