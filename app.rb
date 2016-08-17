320c0e2f6ce6b74df02f98632f1c71e49da029f4
ЛУКАС <lukaswet@gmail.com>
2016-08-17 17:45:30 +0000

    Add validation to visit form

--- app.rb
+++ app.rb
@@ -6,7 +6,10 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-    
+    validates :name, presence: true, length: { minimum: 3 }
+    validates :phone, presence: true
+    validates :datestamp, presence: true
+    validates :color, presence: true
 end
 
 
@@ -24,13 +27,21 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
-	c = Client.new params[:client]
-	c.save
-
-	erb "<h2>Спасибо, вы записались</h2>"
+	@c = Client.new params[:client]
+	
+	if @c.save
+		erb "<h2>Спасибо, вы записались</h2>"
+	else
+		@error = @c.errors.full_messages.first
+		erb :visit
+	end	
 end
 
+get '/barber/:id' do
+	erb :barber
+end
